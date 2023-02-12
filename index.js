6e4ea5b1dbf5440958e25097913bd0bc22727a96
unknown <mdyasirofficial2001@gmail.com>
2023-02-12 12:40:52 +0600

    MongoDB Connected

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -17,3 +17,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bag n Bagz server running in localhost:${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uue1axy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch((e) => console.log(e));
