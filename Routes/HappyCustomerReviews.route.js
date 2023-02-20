cf3212246e02efae2196b843ae31439a18a2d201
unknown <mdyasirofficial2001@gmail.com>
2023-02-20 17:16:26 +0600

    Added Customer Review route to Database.

--- Routes/HappyCustomerReviews.route.js
+++ Routes/HappyCustomerReviews.route.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const HappyCustomerReview = require("../Models/HappyCustomerReviews.model");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.send("Got Happy Customer's Review!!!");
+  try {
+    const result = await HappyCustomerReview.find({}, { __v: false });
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.send("Posted New Review!!!");
+  try {
+    const CustomerReview = new HappyCustomerReview(req.body);
+    const result = await CustomerReview.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+    //Error message for invalid property and key
+    if (error.name === "ValidationError") {
+      return next(createHttpError(422, error.message));
+    }
+    next(error);
+  }
 });
 
 module.exports = router;
